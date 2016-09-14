ae9157a7294fd45027be5ff29976e5d291fb14ef
Björn Pollex <bjoern.pollex@sbgenomics.com>
2016-09-14 15:28:42 +0100

    Formating and naming fixes
     * formating more pep8 compliant
     * conanfiles for packaging don't need a generator
     * less generic name for package class

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,14 @@
-from conans import ConanFile
-import os
-from os import path
+from os import unlink
+from os.path import join
+from conans import CMake, ConanFile
 from conans.tools import download, unzip, check_sha256
-from conans import CMake
 
-class ArbitraryName(ConanFile):
+
+class Catch(ConanFile):
     name = "catch"
     version = "1.5.0"
     branch = "stable"
     license = "Boost"
-    generators = "cmake"
     url="http://github.com/TyRoXx/conan-catch"
 
     ZIP_FOLDER_NAME = "Catch-1.5.0"
@@ -21,13 +20,12 @@ class ArbitraryName(ConanFile):
         download("https://github.com/philsquared/Catch/archive/%s" % self.ZIP_URL_NAME, zip_name)
         check_sha256(zip_name, self.FILE_SHA)
         unzip(zip_name)
-        os.unlink(zip_name)
+        unlink(zip_name)
 
     def package(self):
-        self.copy("catch.hpp", "include", path.join(self.ZIP_FOLDER_NAME, 'single_include'))
-        self.copy("catch_with_main.hpp", "include", path.join(self.ZIP_FOLDER_NAME, 'include'))
+        self.copy("catch.hpp", "include", join(self.ZIP_FOLDER_NAME, 'single_include'))
+        self.copy("catch_with_main.hpp", "include", join(self.ZIP_FOLDER_NAME, 'include'))
 
     def package_info(self):
         self.cpp_info.libdirs = []
         self.cpp_info.resdirs = []
-        
